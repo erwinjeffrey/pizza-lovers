fc89191d3ad439cf8939355b7b718b0c6d693719
ERWIN BASTARDO <erwin_bastardo@bhdleon.com.do>
2018-12-27 11:34:37 -0400

    adding module to organize the feature in the app

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,32 +1,11 @@
-import { SigninComponent } from './auth/signin/signin.component';
-import { NgModule, Component } from '@angular/core';
-import { Routes, RouterModule, Router } from '@angular/router';
-
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import { SignupComponent } from './auth/signup/signup.component';
-import { CanActivate } from '@angular/router/src/utils/preactivation';
-import { AuthGuard } from './auth/auth-guard.service';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-  {
-    path: 'recipes',
-    component: RecipesComponent,
-    children: [
-      { path: '', component: RecipeStartComponent },
-      { path: 'new', component: RecipeEditComponent, canActivate:[AuthGuard] },
-      { path: ':id', component: RecipeDetailComponent },
-      { path: ':id/edit', component: RecipeEditComponent,  canActivate:[AuthGuard] }
-    ]
-  },
-  { path: 'shopping-list', component: ShoppingListComponent },
-  { path: 'signup', component:SignupComponent },
-  { path: 'signin', component:SigninComponent}
+  { path: 'shopping-list', component: ShoppingListComponent }
+
 ];
 @NgModule({
   imports: [RouterModule.forRoot(appRoutes)],
